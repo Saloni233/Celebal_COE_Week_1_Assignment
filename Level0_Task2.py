469780e08ec4c34481fbed441fff2e0c8883daa0
Saloni233 <95487828+Saloni233@users.noreply.github.com>
2024-05-25 19:46:25 +0530

    Add files via upload

---
+++ Level0_Task2.py
@@ -0,0 +1,11 @@
+a = int(input().strip())
+b = int(input().strip())
+
+sum_of_the_two_numbers = a + b
+difference_of_the_two_numbers  = a - b
+product_of_the_two_numbers = a * b
+
+
+print(sum_of_the_two_numbers)
+print(difference_of_the_two_numbers)
+print(product_of_the_two_numbers)
